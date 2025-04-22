abda5e39e7d75b99b7265526df7fd4cd4f99aff5
Nisha <158241806+ahamed-nisha@users.noreply.github.com>
2025-04-21 22:54:29 -0400

    Featured homepage cards limited to three, 'see more' function added

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,26 +1,41 @@
+import { useState } from "react";
+
 const JobListing = ({ job }) => {
+  const [showFull, setShowFull] = useState(false);
+
+  let description = job.description;
+  if (!showFull) {
+    description = description.substring(0, 90) + "...";
+  }
   return (
-    <div class="bg-white rounded-xl shadow-md relative">
-      <div class="p-4">
-        <div class="mb-6">
-          <div class="text-gray-600 my-2">{job.type}</div>
-          <h3 class="text-xl font-bold">{job.title}</h3>
+    <div className="bg-white rounded-xl shadow-md relative">
+      <div className="p-4">
+        <div className="mb-6">
+          <div className="text-gray-600 my-2">{job.type}</div>
+          <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div class="mb-5">{job.description}</div>
+        <div className="mb-5">{description}</div>
 
-        <h3 class="text-indigo-500 mb-2">{job.salary} / Year</h3>
+        <button
+          onClick={() => setShowFull((prevState) => !prevState)}
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+        >
+          {showFull ? "Less" : "More"}
+        </button>
 
-        <div class="border border-gray-100 mb-5"></div>
+        <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
-        <div class="flex flex-col lg:flex-row justify-between mb-4">
-          <div class="text-orange-700 mb-3">
-            <i class="fa-solid fa-location-dot text-lg"></i>
+        <div className="border border-gray-100 mb-5"></div>
+
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
+          <div className="text-orange-700 mb-3">
+            <i className="fa-solid fa-location-dot text-lg"></i>
             {job.location}
           </div>
           <a
             href="{`/job/${job,id}`"
-            class="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+            className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
           </a>
