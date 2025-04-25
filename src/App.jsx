b7e5eb9eb9dc3686976a6226700109c63c0461a9
Nisha <158241806+ahamed-nisha@users.noreply.github.com>
2025-04-24 20:38:51 -0400

    404 Error redirection added

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layout/MainLayout";
 import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route index path="/jobs" element={<JobsPage />} />
+      <Route index path="/*" element={<NotFoundPage />} />
     </Route>
   )
 );
